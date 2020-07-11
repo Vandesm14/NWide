cdc53e6f4433d67588da961fb0458faad00296cd
vandesm14 <vandesm14@gmail.com>
2020-07-11 17:26:16 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// const nw = require('nw.gui');
+let tray;
+
+let winOptions = {
+	icon: 'icon.png',
+	id: 'main'
+};
+
+let win = nw.Window.open('public/index.html', winOptions, (win) => {
+	console.log('Opened window');
+});
